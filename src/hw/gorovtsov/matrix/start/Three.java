890fa3fb675d8f8869aff4cbe730acdd3b367155
gorovtsov <garavek94@gmail.com>
2017-10-26 21:59:09 +0300

    JD01_matrix_6.8.1

--- src/hw/gorovtsov/matrix/start/Three.java
+++ src/hw/gorovtsov/matrix/start/Three.java
@@ -1,3 +1,4 @@
+//6.8.1.3
 package hw.gorovtsov.matrix.start;
 
 import hw.gorovtsov.matrix.logic.Input;
@@ -6,10 +7,11 @@ public class Three {
 
 	public static void main(String[] args) {
 		
-		int n = Input.nextInt(); //
+		int n = Input.nextInt(); //ввожу целое число
 		
-		int[][] ar = new int[n][n];
+		int[][] ar = new int[n][n]; //инициализирую масив
 
+		//заполняю массив значениями
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (i == j) {
@@ -19,6 +21,7 @@ public class Three {
 
 		}
 		
+		//вывожу
 		Input.printMatrix(ar);
 	}
 
